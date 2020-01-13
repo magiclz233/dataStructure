814b1fc9cfd1f1e854907b84426dd1da4382f03d
luozhang <luozhang@cnpc.com.cn>
2020-01-13 14:42:28 +0800

    多线程一些练习

---
+++ src/thread/chapter5/LockTest.java
@@ -0,0 +1,38 @@
+package thread.chapter5;
+
+import java.security.PublicKey;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 罗璋||luozhang@cnpc.com.cn
+ * Description TODO
+ * @version 1.0
+ * @ClassName Lock
+ * @date 2020/1/13 11:49
+ */
+public class LockTest extends Thread {
+
+    private MyService myService;
+
+    public LockTest(MyService service){
+        super();
+        this.myService = service;
+    }
+    @Override
+    public void run() {
+        myService.testLock();
+    }
+
+    public static void main(String[] args){
+      MyService service = new MyService();
+
+      LockTest l1 = new LockTest(service);
+      LockTest l2 = new LockTest(service);
+
+      l1.start();
+      l2.start();
+
+
+    }
+}
---
+++ src/thread/chapter5/Test.java
@@ -0,0 +1,15 @@
+package thread.chapter5;
+
+/**
+ * @author 罗璋||luozhang@cnpc.com.cn
+ * Description TODO
+ * @version 1.0
+ * @ClassName Test
+ * @date 2020/1/13 11:32
+ */
+public class Test {
+    public static void main(String[] args){
+        //队列同步器
+//        AbstractQueuedSynchronizer
+    }
+}
