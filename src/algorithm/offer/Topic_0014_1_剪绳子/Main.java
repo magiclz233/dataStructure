92c20023d0214fa8e83e1cebbe8b435c9d61b4a5
magic <magiclz233@163.com>
2020-09-16 16:57:47 +0800

    offer - 14_1 剪绳子

---
+++ src/algorithm/offer/Topic_0014_1_剪绳子/Main.java
@@ -0,0 +1,28 @@
+package algorithm.offer.Topic_0014_1_剪绳子;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/16 : 14:39
+ */
+public class Main {
+
+    /*
+        给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+        每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+        例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+     */
+
+    public static void main(String[] args) {
+        System.out.println(cuttingRope(21));
+    }
+
+    public static int cuttingRope(int n) {
+        if (n <= 3) return n - 1;
+        int a = n / 3, b = n % 3;
+        if (b == 0) return (int) Math.pow(3, a);
+        if (b == 1) return (int) Math.pow(3, a - 1) * 4;
+        return (int) Math.pow(3, a) * 2;
+    }
+}
