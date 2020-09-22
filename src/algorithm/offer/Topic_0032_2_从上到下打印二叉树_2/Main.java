2ab5947fc8b8f1ea4a0aa6a556260234893281df
magic <magiclz233@163.com>
2020-09-22 17:58:02 +0800

    offer 32_2

---
+++ src/algorithm/offer/Topic_0032_2_从上到下打印二叉树_2/Main.java
@@ -0,0 +1,43 @@
+package algorithm.offer.Topic_0032_2_从上到下打印二叉树_2;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/22 : 9:41
+ */
+public class Main {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> nums = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            nums.add(tmp);
+        }
+        return nums;
+    }
+}
