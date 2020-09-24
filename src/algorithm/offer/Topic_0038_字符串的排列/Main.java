417a0013172edc6add6c824f25d38ca1a81bcda4
magic <magiclz233@163.com>
2020-09-24 08:57:33 +0800

    offer 38

---
+++ src/algorithm/offer/Topic_0038_字符串的排列/Main.java
@@ -0,0 +1,60 @@
+package algorithm.offer.Topic_0038_字符串的排列;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/23 : 9:49
+ */
+public class Main {
+    List<String> res = new LinkedList<>();
+    char[] c;
+    public String[] permutation(String s) {
+        c = s.toCharArray();
+        dfs(0);
+        return res.toArray(new String[res.size()]);
+    }
+
+    void dfs(int x) {
+        if(x == c.length - 1) {
+            // 添加排列方案
+            res.add(String.valueOf(c));
+            return;
+        }
+        HashSet<Character> set = new HashSet<>();
+        for(int i = x; i < c.length; i++) {
+            // 重复，因此剪枝
+            if(set.contains(c[i])) continue;
+            set.add(c[i]);
+            // 交换，将 c[i] 固定在第 x 位
+            swap(i, x);
+            // 开启固定第 x + 1 位字符
+            dfs(x + 1);
+            // 恢复交换
+            swap(i, x);
+        }
+    }
+    void swap(int a, int b) {
+        char tmp = c[a];
+        c[a] = c[b];
+        c[b] = tmp;
+    }
+}
+
+/*
+输入一个字符串，打印出该字符串中字符的所有排列。
+你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ 
+
+示例:
+
+输入：s = "abc"
+输出：["abc","acb","bac","bca","cab","cba"]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof
+ */
