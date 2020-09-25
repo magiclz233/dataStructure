3239928a44d3707bbb0b10c5dc283bf3b412eda3
magic <magiclz233@163.com>
2020-09-25 17:47:09 +0800

    offer 36

---
+++ src/algorithm/offer/Topic_0036_二叉搜索树与双向链表/Main.java
@@ -0,0 +1,46 @@
+package algorithm.offer.Topic_0036_二叉搜索树与双向链表;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/25 : 17:37
+ */
+public class Main {
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    Node pre, head;
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+    void dfs(Node cur) {
+        if(cur == null) return;
+        dfs(cur.left);
+        if(pre != null) pre.right = cur;
+        else head = cur;
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
+    }
+}
