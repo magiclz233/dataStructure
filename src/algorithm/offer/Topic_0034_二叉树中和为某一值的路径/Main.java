167439f30a4e6a71e8ac34664e5d40d7139577c3
magic <magiclz233@163.com>
2020-09-25 17:47:02 +0800

    offer 34

---
+++ src/algorithm/offer/Topic_0034_二叉树中和为某一值的路径/Main.java
@@ -0,0 +1,58 @@
+package algorithm.offer.Topic_0034_二叉树中和为某一值的路径;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/25 : 17:02
+ */
+public class Main {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(root, sum, new ArrayList<>(), result);
+        return result;
+    }
+
+    void dfs(TreeNode root, int sum, List<Integer> list, List<List<Integer>> result) {
+        if (root == null) return;
+        list.add(root.val);
+        //如果到达叶子节点，就不能往下走了，直接return
+        if (root.left == null && root.right == null) {
+            //如果到达叶子节点，并且sum等于叶子节点的值，说明我们找到了一组，
+            //要把它放到result中
+            if (sum == root.val) {
+                result.add(new ArrayList<>(list));
+            }
+            //注意别忘了把最后加入的结点值给移除掉，因为下一步直接return了，
+            //不会再走最后一行的remove了，所以这里在rerurn之前提前把最后
+            //一个结点的值给remove掉。
+            list.remove(list.size() - 1);
+            //到叶子节点之后直接返回，因为在往下就走不动了
+            return;
+        }
+        //如果没到达叶子节点，就继续从他的左右两个子节点往下找，注意到
+        //下一步的时候，sum值要减去当前节点的值
+        dfs(root.left, sum - root.val, list, result);
+        dfs(root.right, sum - root.val, list, result);
+        //我们要理解递归的本质，当递归往下传递的时候他最后还是会往回走，
+        //我们把这个值使用完之后还要把它给移除，这就是回溯
+        list.remove(list.size()-1);
+    }
+
+
+}
