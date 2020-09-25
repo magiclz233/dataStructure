b196f3ecf8937c2f44b82c2b53e5b9b4c3b7bf67
magic <magiclz233@163.com>
2020-09-25 18:08:26 +0800

    offer 44

---
+++ src/algorithm/offer/Topic_0044_数字序列中某一位的数字/Main.java
@@ -0,0 +1,30 @@
+package algorithm.offer.Topic_0044_数字序列中某一位的数字;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname Main
+ * @date 2020/9/25 : 18:03
+ */
+public class Main {
+    public int findNthDigit(int n) {
+        return 1;
+    }
+}
+
+/*
+数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，等等。
+请写一个函数，求任意第n位对应的数字。
+
+示例 1：
+
+输入：n = 3
+输出：3
+示例 2：
+
+输入：n = 11
+输出：0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof
+ */
