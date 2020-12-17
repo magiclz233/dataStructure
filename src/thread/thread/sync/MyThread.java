016451777bbc2401f9ceff4f3659309d8063d970
magic <magiclz233@163.com>
2020-12-17 18:05:36 +0800

    synchronized

---
+++ src/thread/thread/sync/MyThread.java
@@ -0,0 +1,25 @@
+package thread.thread.sync;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname MyThread
+ * @date 2020/12/17 : 17:49
+ */
+public class MyThread implements Runnable {
+
+    public Integer i = 100;
+    @Override
+    public void run() {
+        test();
+    }
+
+
+    public void test(){
+        synchronized (MyThread.class){
+            i--;
+            System.out.println(Thread.currentThread().getName()+"卖出去一张：剩余数量："+i);
+
+        }
+    }
+}
