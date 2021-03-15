4bb49cf755fb334a3e2e30d39c4f785642a49952
magic <magiclz233@163.com>
2021-03-15 17:57:51 +0800

    布隆过滤器-01

---
+++ src/algorithm/bloom/BloomFilter.java
@@ -0,0 +1,13 @@
+package algorithm.bloom;
+
+import java.io.Serializable;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname BloomFilter
+ * @date 2021/2/20 : 9:20
+ */
+public class BloomFilter implements Serializable {
+    private static final long serialVersionUID = -5221305273707291280L;
+}
