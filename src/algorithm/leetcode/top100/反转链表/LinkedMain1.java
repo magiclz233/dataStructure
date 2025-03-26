c87cb3e4a5937def880c9156a3003483a55eabd9
magiclz233 <magiclz233@163.com>
2025-03-26 17:42:03 +0800

    feat: 在LinkedMain1.java中添加头插法反转链表方法
    添加了`reverseList1`方法，使用头插法实现链表反转，以提高代码的可读性和维护性

--- src/algorithm/leetcode/top100/反转链表/LinkedMain1.java
+++ src/algorithm/leetcode/top100/反转链表/LinkedMain1.java
@@ -21,4 +21,18 @@ public class LinkedMain1 {
         
         return prev; // 最后prev就是新的头节点
     }
+
+    // 头插法
+    public ListNode reverseList1(ListNode head) {
+        ListNode newHead = null;
+        
+        while (head != null) {
+            ListNode nextNode = head.next; // 保存下一个节点
+            head.next = newHead;          // 当前节点插入到新链表头部
+            newHead = head;               // 更新新链表头
+            head = nextNode;              // 移动到原链表下一个节点
+        }
+        
+        return newHead;
+    }
 }
