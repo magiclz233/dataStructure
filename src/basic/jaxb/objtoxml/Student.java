fcf21950a7e5c97453891e7fc07f5407e083d641
magiclz233 <magiclz233@163.com>
2025-03-28 17:53:59 +0800

    refactor(basic): 删除未使用的 JAXB 相关代码
    -移除了 ObjXmlUtils.java 和 Student.java 文件中的所有代码
    -这些类不再使用 JAXB 进行 XML 和 Java 对象之间的转换

--- src/basic/jaxb/objtoxml/Student.java
+++ src/basic/jaxb/objtoxml/Student.java
@@ -1,50 +1,50 @@
-package basic.jaxb.objtoxml;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- * @author 罗璋|luozhang@cnpc.com.cn
- * @version 1.0
- * @classname Student
- * @description TODO
- * @date 2020/2/23 14:42
- */
-@XmlRootElement
-public class Student {
-    private String id;
-    private String name;
-    private Integer age;
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-}
+//package basic.jaxb.objtoxml;
+//
+//import javax.xml.bind.annotation.XmlRootElement;
+//
+///**
+// * @author 罗璋|luozhang@cnpc.com.cn
+// * @version 1.0
+// * @classname Student
+// * @description TODO
+// * @date 2020/2/23 14:42
+// */
+//@XmlRootElement
+//public class Student {
+//    private String id;
+//    private String name;
+//    private Integer age;
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "id='" + id + '\'' +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//}
